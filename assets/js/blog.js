66362f703fd8d1327b9d7a8ccf2543b97b9e78e0
Jake R <jakerasmusson@gmail.com>
2024-03-19 09:53:33 -0500

    adding darkmode

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,2 +1,45 @@
 let data = JSON.parse(localStorage.getItem('blog'))
 console.log(data)
+let section = document.querySelector('section')
+
+
+data.forEach(blog => {
+    let author = blog.authName;
+    let title = blog.title;
+    let content = blog.content;
+    console.log(author, title, content)
+    let authH2 = document.createElement('h2')
+    let titleH3 = document.createElement('h3')
+    let contentP = document.createElement('p')
+    authH2.textContent = author
+    titleH3.textContent = title
+    contentP.textContent = content
+    console.log(authH2, titleH3 , contentP)
+    authH2.style.textAlign = 'center';
+    authH2.style.width = '100%';
+    authH2.style.border = '5px solid blue';
+    authH2.style.fontSize = '35px'
+    authH2.style.margin = '20px 0'
+    titleH3.style.marginBottom = '15px'
+    titleH3.style.textAlign = 'center';
+    titleH3.style.width = '100%';
+    titleH3.style.border = '5px solid blue';
+    titleH3.style.borderStyle = 'hidden hidden solid';
+    titleH3.style.fontSize = '35px'
+    // titleH3.style.marginBottom = '-100px'
+    contentP.style.fontSize = '20px'
+    contentP.style.textAlign = 'center'
+
+   section.appendChild(authH2)
+   section.appendChild(titleH3)
+   section.appendChild(contentP)
+});
+
+
+
+
+darkAndBrooding.onclick = function() {
+    var elem = document.body
+    elem.classList.toggle('dark-mode')
+
+}
--- assets/js/form.js
+++ assets/js/form.js
@@ -9,12 +9,14 @@ function gatherBlog() {
     const content = document.getElementById('content').value;
     if (authName === '' || title === '' || content === '') {
         alert('Please fill out all areas!')
+        form.reset();
         return
     } else {
         blogPost.push({authName, title, content})
         localStorage.setItem('blog', JSON.stringify(blogPost))
         let data = JSON.parse(localStorage.getItem('blog'))
         console.log(data)
+        form.reset();
         
     }
 }
