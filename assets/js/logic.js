56b0273fa03bbbb0c447a32d05648a9bacfc92e9
Jake R <jakerasmusson@gmail.com>
2024-03-19 14:16:09 -0500

    makes darkmode persistent

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,27 @@
-darkAndBrooding.onclick = function() {
-    var elem = document.body
+const darkModeToggle = document.getElementById('darkAndBrooding');
+const elem = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+function setDarkMode() {
     elem.classList.toggle('dark-mode')
+    localStorage.setItem('dark-mode', 'true')
+};
+
+function removeDarkMode() {
+    elem.classList.toggle('dark-mode')
+    localStorage.setItem('dark-mode', 'false')
+};
+
+if (darkMode === 'true') {
+    setDarkMode();
+}
+
+darkModeToggle.onclick = function() {
+    let darkModeStore = localStorage.getItem('dark-mode');
+    if (darkModeStore == 'false') {
+        setDarkMode();
+    } else {
+        removeDarkMode();
+    }
 
 }
